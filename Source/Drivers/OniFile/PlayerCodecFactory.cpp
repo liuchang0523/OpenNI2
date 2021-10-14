1e3c6f118170681c841900ac68a51fb9b125360d
liuchang <liuchang@orbbec.com>
2021-10-14 09:59:20 +0800

    去掉对编码格式的校验

--- Source/Drivers/OniFile/PlayerCodecFactory.cpp
+++ Source/Drivers/OniFile/PlayerCodecFactory.cpp
@@ -32,97 +32,97 @@
 
 namespace oni_file {
 
-//---------------------------------------------------------------------------
-// Code
-//---------------------------------------------------------------------------
-XnStatus PlayerCodecFactory::Create(XnCodecID nCodecID, PlayerSource* pSource, XnCodec** ppCodec)
-{
-	OniStatus rc;
-	XnCodec* pCodec = NULL;
+    //---------------------------------------------------------------------------
+    // Code
+    //---------------------------------------------------------------------------
+    XnStatus PlayerCodecFactory::Create(XnCodecID nCodecID, PlayerSource* pSource, XnCodec** ppCodec)
+    {
+        OniStatus rc;
+        XnCodec* pCodec = NULL;
 
-	switch (nCodecID)
-	{
-		case XN_CODEC_UNCOMPRESSED:
-		{
-			XN_VALIDATE_NEW_AND_INIT(pCodec, XnUncompressedCodec);
-			break;
-		}
-		case XN_CODEC_16Z:
-		{
-			XN_VALIDATE_NEW_AND_INIT(pCodec, Xn16zCodec);
-			break;
-		}
-		case XN_CODEC_16Z_EMB_TABLES:
-		{
-			// first we need to find max depth
-			int nMaxDepth;
-			int dataSize = sizeof(nMaxDepth);
-			rc = pSource->GetProperty(ONI_STREAM_PROPERTY_MAX_VALUE, &nMaxDepth, &dataSize);
-			if (rc != ONI_STATUS_OK)
-			{
-				return XN_STATUS_ERROR;
-			}
+        switch (nCodecID)
+        {
+        case XN_CODEC_UNCOMPRESSED:
+        {
+            XN_VALIDATE_NEW_AND_INIT(pCodec, XnUncompressedCodec);
+            break;
+        }
+        case XN_CODEC_16Z:
+        {
+            XN_VALIDATE_NEW_AND_INIT(pCodec, Xn16zCodec);
+            break;
+        }
+        case XN_CODEC_16Z_EMB_TABLES:
+        {
+            // first we need to find max depth
+            int nMaxDepth;
+            int dataSize = sizeof(nMaxDepth);
+            rc = pSource->GetProperty(ONI_STREAM_PROPERTY_MAX_VALUE, &nMaxDepth, &dataSize);
+            if (rc != ONI_STATUS_OK)
+            {
+                return XN_STATUS_ERROR;
+            }
 
-			XN_VALIDATE_NEW_AND_INIT(pCodec, Xn16zEmbTablesCodec, (OniDepthPixel)nMaxDepth);
-			break;
-		}
-		case XN_CODEC_8Z:
-		{
-			XN_VALIDATE_NEW_AND_INIT(pCodec, Xn8zCodec);
-			break;
-		}
-		case XN_CODEC_JPEG:
-		{
-			// check what is the output format
-			OniVideoMode videoMode;
-			int dataSize = sizeof(videoMode);
-			rc = pSource->GetProperty(ONI_STREAM_PROPERTY_VIDEO_MODE, &videoMode, &dataSize);
-			if (rc != ONI_STATUS_OK)
-			{
-				return XN_STATUS_ERROR;
-			}
+            XN_VALIDATE_NEW_AND_INIT(pCodec, Xn16zEmbTablesCodec, (OniDepthPixel)nMaxDepth);
+            break;
+        }
+        case XN_CODEC_8Z:
+        {
+            XN_VALIDATE_NEW_AND_INIT(pCodec, Xn8zCodec);
+            break;
+        }
+        case XN_CODEC_JPEG:
+        {
+            // check what is the output format
+            OniVideoMode videoMode;
+            int dataSize = sizeof(videoMode);
+            rc = pSource->GetProperty(ONI_STREAM_PROPERTY_VIDEO_MODE, &videoMode, &dataSize);
+            if (rc != ONI_STATUS_OK)
+            {
+                return XN_STATUS_ERROR;
+            }
 
-			XnBool bRGB = FALSE;
+            XnBool bRGB = FALSE;
 
-			switch (videoMode.pixelFormat)
-			{
-				case ONI_PIXEL_FORMAT_GRAY8:
-				{
-					bRGB = FALSE;
-					break;
-				}
-				case ONI_PIXEL_FORMAT_RGB888:
-				{
-					bRGB = TRUE;
-					break;
-				}
-				default:
-				{
-					//XN_LOG_WARNING_RETURN(XN_STATUS_ERROR, XN_MASK_DDK, "Codec factory currently supports JPEG codec only for streams of type Gray8 or RGB24!");
-					return XN_STATUS_ERROR;
-				}
-			}
+            //switch (videoMode.pixelFormat)
+            //{
+            //	case ONI_PIXEL_FORMAT_GRAY8:
+            //	{
+            //		bRGB = FALSE;
+            //		break;
+            //	}
+            //	case ONI_PIXEL_FORMAT_RGB888:
+            //	{
+            //		bRGB = TRUE;
+            //		break;
+            //	}
+            //	default:
+            //	{
+            //		//XN_LOG_WARNING_RETURN(XN_STATUS_ERROR, XN_MASK_DDK, "Codec factory currently supports JPEG codec only for streams of type Gray8 or RGB24!");
+            //		return XN_STATUS_ERROR;
+            //	}
+            //}
 
-			// take X and Y res
-			XnUInt32 nXRes = videoMode.resolutionX;
-			XnUInt32 nYRes = videoMode.resolutionY;
-			XN_VALIDATE_NEW_AND_INIT(pCodec, XnJpegCodec, bRGB, nXRes, nYRes);
-			break;
-		}
-		default:
-		{
-			//XN_LOG_WARNING_RETURN(XN_STATUS_ERROR, XN_MASK_DDK, "Codec factory does not support compression type %d", nFormat);
-			return XN_STATUS_ERROR;
-		}
-	}
+            // take X and Y res
+            XnUInt32 nXRes = videoMode.resolutionX;
+            XnUInt32 nYRes = videoMode.resolutionY;
+            XN_VALIDATE_NEW_AND_INIT(pCodec, XnJpegCodec, bRGB, nXRes, nYRes);
+            break;
+        }
+        default:
+        {
+            //XN_LOG_WARNING_RETURN(XN_STATUS_ERROR, XN_MASK_DDK, "Codec factory does not support compression type %d", nFormat);
+            return XN_STATUS_ERROR;
+        }
+        }
 
-	*ppCodec = pCodec;
-	return (XN_STATUS_OK);
-}
+        *ppCodec = pCodec;
+        return (XN_STATUS_OK);
+    }
 
-void PlayerCodecFactory::Destroy(XnCodec* pCodec)
-{
-	XN_DELETE(pCodec);
-}
+    void PlayerCodecFactory::Destroy(XnCodec* pCodec)
+    {
+        XN_DELETE(pCodec);
+    }
 
 } // namespace oni_files_player
